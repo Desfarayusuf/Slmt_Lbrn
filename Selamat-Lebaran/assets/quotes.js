096bb0a9b972a05d74b8a62414dd79a4c374a8fd
Dsvrys <60470765+Desfarayusuf@users.noreply.github.com>
2020-05-16 20:18:09 +0700

    Add files via upload

---
+++ Selamat-Lebaran/assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "by: @Ysf", quotes: "Selamat Hari Raya Idul FItri..." },
+   /* { author: "by: @Ysf", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
+    { author: "by: @Ysf", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "by: @Ysf", quotes: "I love you." },
+    { author: "by: @Ysf", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "by: @Ysf", quotes: "I love you." },
+    { author: "by: @Ysf", quotes: "I'm sorry for loving you." },*/
+];
---
+++ Selamat-Lebaran/assets/variables.js
@@ -0,0 +1,10 @@
+export default {
+  nicknames: ["",],
+  greetings: {
+    evening: "Selamat Malam ",
+    afternoon: "Selamat Sore",
+    day: "Siang....",
+    morning: "Selamat Pagi",
+    night: "Selamat Malam"
+  }
+};
